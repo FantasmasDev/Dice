a495ece68254b0666ec70e2ae3619656f1f264d6
FantasmasDe <fantasmas.dev@gmail.com>
2023-09-01 00:32:49 +0300

    Переписано под два кубика

--- app/src/main/java/com/fantasmas/dice/MainActivity.kt
+++ app/src/main/java/com/fantasmas/dice/MainActivity.kt
@@ -9,16 +9,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
-        }
+        val resultRoll: TextView = findViewById(R.id.textView)
+        val text = "${dice.roll()} ${dice.roll()}"
+        resultRoll.text = text
+    }
 }
 
 class Dice(val numSides: Int) {

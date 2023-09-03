95f7a96001963dfe33a1aa945b45e23b0a847048
FantasmasDe <fantasmas.dev@gmail.com>
2023-09-03 23:48:18 +0300

    Переписано под два кубика

--- app/src/main/java/com/fantasmas/dice/MainActivity.kt
+++ app/src/main/java/com/fantasmas/dice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.fantasmas.dice
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,14 +16,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice = Dice(6)
-        val resultRoll: TextView = findViewById(R.id.textView)
-        val text = "${dice.roll()} ${dice.roll()}"
-        resultRoll.text = text
+        val firstDice = Dice(6)
+        val secondDice = Dice(6)
+        val imageFirstRoll: ImageView = findViewById(R.id.firstDice)
+        val imageSecondRoll: ImageView = findViewById(R.id.secondDice)
+
+        fun setDrawable(dice: Dice, image: ImageView){
+            when (dice.roll()) {
+                1 -> image.setImageResource(R.drawable.dice_1)
+                2 -> image.setImageResource(R.drawable.dice_2)
+                3 -> image.setImageResource(R.drawable.dice_3)
+                4 -> image.setImageResource(R.drawable.dice_4)
+                5 -> image.setImageResource(R.drawable.dice_5)
+                6 -> image.setImageResource(R.drawable.dice_6)
+            }
+        }
+
+        setDrawable(firstDice, imageFirstRoll)
+        setDrawable(secondDice, imageSecondRoll)
     }
 }
 
-class Dice(val numSides: Int) {
+class Dice(private val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
     }
